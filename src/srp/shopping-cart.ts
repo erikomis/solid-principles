0033b0a8ace8789f387463a22f39f17b49a25dd4
erikmartins <erik.ozzetti.martins@gmail.com>
2023-01-13 18:51:24 -0300

    feat :  class shopping cart principles srp

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -1,8 +1,7 @@
-type CartItem = { name: string; price: number };
+import { CartItem } from './interfaces/cart-item';
 
-export class ShopingCart {
+export class ShoppingCart {
   private readonly _items: CartItem[] = [];
-  private orderStatus: 'open' | 'closed' = 'open';
 
   addItem(item: CartItem): void {
     this._items.push(item);
@@ -16,35 +15,14 @@ export class ShopingCart {
     return this._items;
   }
 
-  get total(): number {
+  total(): number {
     return +this._items
       .reduce((total, next) => total + next.price, 0)
       .toFixed(2);
   }
 
-  checkout(): void {
-    if (this.isEmpty) {
-      console.log('Seu carrinho está vazio');
-      return;
-    }
-
-    console.log('Seu pedido foi recebido.');
-    this.orderStatus = 'closed';
-    this.sendMessage(`Seu pedido com total de ${this.total} foi recebido.`);
-    this.saveOrder();
-    this.clear();
-  }
-
-  get isEmpty(): boolean {
+  isEmpty(): boolean {
     return this._items.length === 0;
-  }
-
-  sendMessage(msg: string): void {
-    console.log('Mensagem enviada:', msg);
-  }
-
-  saveOrder(): void {
-    console.log('Pedido salvo com sucesso...');
   }
 
   clear(): void {
@@ -52,6 +30,3 @@ export class ShopingCart {
     this._items.length = 0;
   }
 }
-
-const cart = new ShopingCart();
-cart.addItem({ name: 'Camiseta', price: 49.9 });
